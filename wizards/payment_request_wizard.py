901786ae0b11bb06488553f5be89932de8e076e8
Ali Alhashim <ali-alhashim@outlook.com>
2026-02-04 19:20:38 +0300

    fixed security for wizard

--- wizards/payment_request_wizard.py
+++ wizards/payment_request_wizard.py
@@ -4,7 +4,8 @@ class ISPPaymentRequestWizard(models.TransientModel):
     _name = 'isp.payment.request.wizard'
 
 
-    department_id = fields.Many2one('hr.department', required=True)
+    department_id = fields.Many2one('hr.department', string="Department", tracking=True)
+    employee_id = fields.Many2one('hr.employee', string="Assigned Employee", tracking=True)
     date_from = fields.Date(required=True)
     date_to = fields.Date(required=True)
 
